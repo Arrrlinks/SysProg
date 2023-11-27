2b316f87e57d673a402adec599570d8c42dbd906
Matth2A <matthis.pinheirocruz@gmail.com>
2023-11-27 21:31:47 +0100

    Feature : Architecture and start of the project upd : View Model ViewModel Program

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Purpose: Entry point for the program
+
+using EasySave.ViewName;
+using EasySave.ModelName;
+using EasySave.ViewModelName;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Variable Initialisation
+            View view = new View();
+            ViewModel viewModel = new ViewModel(view);
+            //Ask the mode the user wants
+            string mode = viewModel.GetMode();
+
+            switch (mode)
+            {
+                case "COPY":
+                    Console.WriteLine("copy");
+                    break;
+
+                case "HISTORY":
+                    Console.WriteLine("HISTORY");
+                    break;
+
+                case "EXIT":
+                    Environment.Exit(1);
+                    break;
+            }
+        }
+    }
+}
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EasySave.ViewName;
+
+
+namespace EasySave.ViewModelName
+{
+    public class ViewModel
+    {
+        //Builders
+        private View _view;
+
+        public ViewModel() 
+        {
+            _view = new View();
+        }
+        public ViewModel(View view)
+        {
+            _view = view;
+        }
+
+        //Get the mode the user want 
+        public string GetMode()
+        {
+            string? mode = _view.GetMode().ToUpper();
+            while (mode != "COPY" && mode != "HISTORY" && mode != "EXIT")
+            {
+                Console.Clear();
+                Console.WriteLine("You typed a wrong imput. Try again");
+                mode = _view.GetMode().ToUpper();
+            }
+            return mode;
+        }
+
+
+
+    }
+}
