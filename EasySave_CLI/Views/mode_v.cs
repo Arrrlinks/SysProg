545d1a881746a5e70ed06df106ba84fc60b228c7
Matth2A <matthis.pinheirocruz@gmail.com>
2023-11-28 12:15:05 +0100

    upd: changed code structure

---
+++ EasySave_CLI/Views/mode_v.cs
@@ -0,0 +1,14 @@
+﻿namespace EasySave_CLI.Views;
+
+public class mode_v
+{
+    public mode_v() {}
+    
+    //Get the mode the user want 
+    public string? GetMode()
+    {
+        Console.WriteLine("Select the mode \n - Copy \n - History \n - Exit");
+        string? mode = Console.ReadLine();
+        return mode;
+    }
+}
