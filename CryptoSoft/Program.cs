d7f89fbbbfbded39b09051e34b2d5443b21bbb57
Matth2A <matthis.pinheirocruz@gmail.com>
2023-12-08 09:33:09 +0100

    upd: CryptoSoft MVVM

--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -6,40 +6,24 @@ namespace CryptoSoft
 {
     class Program
     {
+        static ViewModel _viewModel;
+
         static void Main(string[] args)
         {
-            
-            //initialise variables
-            string path = args[0];
-            string key = args[1];;
-            string text = "";
+            if (args.Length == 0)
+            {
+                _viewModel = new ViewModel();
+            }
             //if not the good number of arguments, quit
-            if (args.Length != 2)
+            else if (args.Length != 2)
             {
                 throw new ArgumentNullException("Not enough arguments");
             }
-            //if the key is more than 64 bytes or null, quit
-            if (key.Length == 0 | key.Length >32)
-            {
-                throw new ArgumentException("Byte must be 8 bits long");
-            }
-            
-            int Index = 0;
-            if (File.Exists(path))
-            {
-                //for each character in the file
-                foreach (char character in File.ReadAllText(path))
-                {
-                    text += (char) (character^key[Index]);
-                    Index = (Index+1) % key.Length;
-                }
-            }
             else
             {
-                //if not, quit
-                throw new FileNotFoundException("File not found");
+                _viewModel = new ViewModel(args[0], args[1]);
             }
-            File.WriteAllText(path, text);
+            _viewModel.Run();
         }
     }
 }
---
+++ CryptoSoft/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CryptoSoft;
+using CryptoSoft;
+namespace CryptoSoft;
+
+public class ViewModel
+{
+    private View _view;
+    private Model _model;
+    private string _path;
+    private string _key;
+
+    public ViewModel()
+    {
+        _path = "";
+        _key = "";
+        _view = new View();
+        _model = new Model();
+    }
+    
+    public ViewModel(string path, string key)
+    {
+        _path = path;
+        _key = key;
+        _view = new View();
+        _model = new Model();
+    }
+    
+    public void SetPath()
+    {
+        _path = _view.GetPath();
+    }
+    
+    public void SetKey()
+    {
+        _key = _view.GetKey();
+    }
+    
+    public void Encrypt()
+    {
+        _model.encrypt(_path, _key);
+    }
+
+    public void Run()
+    {
+        //if the path or the key is null, get them
+        if (_path.Length == 0)
+        {
+            _path = _view.GetPath();
+        }
+        if (_key.Length == 0)
+        {
+            _key = _view.GetKey();
+        }
+        Encrypt();
+    }
+    
+}
