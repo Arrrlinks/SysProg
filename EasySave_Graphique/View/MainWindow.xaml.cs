493922e1b8fd99218c158483985a0180f0f8f7d8
Matth2A <matthis.pinheirocruz@gmail.com>
2023-12-09 18:17:10 +0100

    Feat: all pages + MVVM architecture upd:all pages

--- EasySave_Graphique/View/MainWindow.xaml.cs
+++ EasySave_Graphique/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Windowsform = System.Windows.Forms;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +8,27 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EasySave_Graphique
 {
     /// <summary>
-    /// Logique d'interaction pour Button.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Button : Page
+    public partial class MainWindow : Window
     {
-        public Button()
+        public MainWindow()
         {
             InitializeComponent();
         }
     }
+    
 }
+
