4bf5fe51618c851a45f03a4f852114df96d94cbe
Matth2A <matthis.pinheirocruz@gmail.com>
2023-12-10 23:12:22 +0100

    upd: remove

--- EasySave_Graphique/ViewModels/Modify_vm.cs
+++ EasySave_Graphique/ViewModels/Modify_vm.cs
@@ -12,14 +12,21 @@ public class Modify_vm : Base_vm
     public ObservableCollection<backup> Backups { get; set; } //list of backups that update UI
     
     public RelayCommand AddCommand {get; set;}
+    public RelayCommand RemoveCommand {get; set;}
     
     private backup _selectedBackup;
     
     //builder
     public Modify_vm()
     {
+        //List of backups
         Backups = new ObservableCollection<backup>();
-        AddCommand = new RelayCommand(execute => AddBackup());
+        //commands
+        
+        //relais command for the buttons
+        AddCommand = new RelayCommand(execute => ModifyBackup());
+        RemoveCommand = new RelayCommand(execute => removeBackup());
+        
         Backups.Add(new backup
         {
             Name = "save1",
@@ -41,18 +48,32 @@ public class Modify_vm : Base_vm
             OnPropertyChanged();
         }
     }
-
-    private void AddBackup()
+    
+    private void removeBackup()
     {
-        Backups.Add(new backup
+        Backups.Remove(SelectedBackup);
+    }
+    private void ModifyBackup()
+    {
+        int i = 0;
+        //modify the selected backup
+        foreach (var backup in Backups)
         {
-            Name = "save1",
-            Source = "C:/Users/Utilisateur/Desktop/Source",
-            Target = "C:/Users/Utilisateur/Desktop/Target",
-            Date = "01/01/2021",
-            Size = "100Mo",
-            filesNB = "10", 
-            State = "Success"
-        });
+            if (backup.Name == SelectedBackup.Name)
+            {
+                i = 1;
+                backup.Source = SelectedBackup.Source;
+                backup.Target = SelectedBackup.Target;
+                backup.Date = SelectedBackup.Date;
+                backup.Size = SelectedBackup.Size;
+                backup.filesNB = SelectedBackup.filesNB;
+                backup.State = SelectedBackup.State;
+            }
+        }
+        if (i==0)
+        {
+            Backups.Add(SelectedBackup);
+        }
+        
     }
 }
