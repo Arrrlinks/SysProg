3c0f366fe10c2d000c7951151e169d8ea3a24ca6
Matth2A <matthis.pinheirocruz@gmail.com>
2023-12-10 15:59:01 +0100

    fix: fix architecture

---
+++ EasySave_Graphique/View/SaveView/Save.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EasySave_Graphique.View.SaveView;
+
+public partial class Save : UserControl
+{
+    public Save()
+    {
+        InitializeComponent();
+    }
+    
+    private void UIElement_OnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.OriginalSource is CheckBox)
+        {
+            // Laisser la sélection se produire
+        }
+        else
+        {
+            // Annuler la sélection en empêchant la propagation de l'événement
+            e.Handled = true;
+        }
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+    }
+    
+}
