1320826b0f4e74fbbd3c0d7a34ccd3e65e76ac4b
Matth2A <matthis.pinheirocruz@gmail.com>
2023-12-11 00:40:05 +0100

    upd: back to another commit part 2

---
+++ EasySave_Graphique/Models/settings_m.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace EasySave_Graphique.Models;
+
+public class Settings_m
+{
+    public void UpdateConfigFile(string key, string value)
+    {
+        string configFilePath = "../../../config.json";
+        List<Dictionary<string, object>> config;
+        string configJson;
+        if (!File.Exists(configFilePath) || string.IsNullOrWhiteSpace(File.ReadAllText(configFilePath)))
+        {
+            config = new List<Dictionary<string, object>>
+            {
+                new Dictionary<string, object> { { "Name", "Lang" }, { "Lang", "fr" } },
+                new Dictionary<string, object> { { "Name", "Format" }, { "Format", "json" } },
+                new Dictionary<string, object> { { "Name", "Extensions" }, { "Extensions", new List<string> { "txt", "json" } } }
+            };
+        }
+        else
+        {
+            try
+            {
+                configJson = File.ReadAllText(configFilePath);
+                config = JsonSerializer.Deserialize<List<Dictionary<string, object>>>(configJson);
+            }
+            catch (JsonException)
+            {
+                config = new List<Dictionary<string, object>>
+                {
+                    new Dictionary<string, object> { { "Name", "Lang" }, { "Lang", "fr" } },
+                    new Dictionary<string, object> { { "Name", "Format" }, { "Format", "json" } },
+                    new Dictionary<string, object> { { "Name", "Extensions" }, { "Extensions", new List<string> { "txt", "json" } } }
+                };
+            }
+        }
+        var item = config.Find(dict => dict.ContainsKey("Name") && dict["Name"].ToString() == key);
+
+        if (item != null)
+        {
+            if (key == "Lang" || key == "Format")
+            {
+                item[key] = value;
+            }
+            else if (key == "Extensions")
+            {
+                item[key] = value.Split(',').ToList();
+            }
+        }
+        configJson = JsonSerializer.Serialize(config);
+        File.WriteAllText(configFilePath, configJson);
+    }
+}
---
+++ EasySave_Graphique/ViewModels/Modify_vm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using EasySave_Graphique.Models;
+
+namespace EasySave_Graphique.ViewModels;
+
+public class Modify_vm : Base_vm
+{
+    //attributs
+    public ObservableCollection<backup> Backups { get; set; } //list of backups that update UI
+    
+    public RelayCommand AddCommand {get; set;}
+    public RelayCommand RemoveCommand {get; set;}
+    
+    private backup _selectedBackup;
+    
+    //builder
+    public Modify_vm()
+    {
+        //List of backups
+        Backups = new ObservableCollection<backup>();
+        //commands
+        
+        //relais command for the buttons
+        AddCommand = new RelayCommand(execute => ModifyBackup());
+        RemoveCommand = new RelayCommand(execute => removeBackup());
+        
+        Backups.Add(new backup
+        {
+            Name = "save1",
+            Source = "C:/Users/Utilisateur/Desktop/Source",
+            Target = "C:/Users/Utilisateur/Desktop/Target",
+            Date = "01/01/2021",
+            Size = "100Mo",
+            filesNB = "10", 
+            State = "Success"
+        });
+    }
+    //methods
+    public backup SelectedBackup
+    {
+        get { return _selectedBackup; }
+        set
+        {
+            _selectedBackup = value; 
+            OnPropertyChanged();
+        }
+    }
+    
+    private void removeBackup()
+    {
+        Backups.Remove(SelectedBackup);
+    }
+    private void ModifyBackup()
+    {
+        int i = 0;
+        //modify the selected backup
+        foreach (var backup in Backups)
+        {
+            if (backup.Name == SelectedBackup.Name)
+            {
+                i = 1;
+                backup.Source = SelectedBackup.Source;
+                backup.Target = SelectedBackup.Target;
+                backup.Date = SelectedBackup.Date;
+                backup.Size = SelectedBackup.Size;
+                backup.filesNB = SelectedBackup.filesNB;
+                backup.State = SelectedBackup.State;
+            }
+        }
+        if (i==0)
+        {
+            Backups.Add(SelectedBackup);
+        }
+        
+    }
+}
